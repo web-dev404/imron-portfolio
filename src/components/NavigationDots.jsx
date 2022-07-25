4ade2d5bf39feccf1c053dd8518e2a9d12fce49f
Imron <imronimronov32@gmail.com>
2022-07-26 00:29:19 +0500

    initial commit

---
+++ src/components/NavigationDots.jsx
@@ -0,0 +1,14 @@
+"use strict";
+import React from 'react';
+
+const NavigationDots = ({active}) => {
+    return (
+        <div className={'app__navigation'}>
+            {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map((item, index) => (
+                <a className={'app__navigation-dot'}
+                   href={`#${item}`} key={item + index} style={active === item ? {backgroundColor: '#313BAC'} : {}}/>
+            ))}
+        </div>
+    )
+}
+export default NavigationDots;
---
+++ src/container/Testimonial/Testimonial.jsx
@@ -0,0 +1,87 @@
+"use strict";
+import React, {useEffect, useState} from 'react';
+import './Testimonials.scss'
+import {motion} from 'framer-motion'
+import {AppWrap, MotionWrap} from "../../wrapper";
+import {urlFor, client} from "../../client";
+import {HiChevronLeft, HiChevronRight} from "react-icons/hi";
+
+// Swiper
+import { Navigation, Pagination, Scrollbar, A11y} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const Testimonial = () => {
+    const [brands, setBrands] = useState([]);
+    const [testimonials, setTestimonials] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleClick = (index) => {
+        setCurrentIndex(index);
+    }
+
+    useEffect(() => {
+        const query = '*[_type == "testimonials"]';
+        const brandsQuery = '*[_type == "brands"]';
+
+        client.fetch(query)
+            .then((data) => {
+                setTestimonials(data);
+            });
+
+        client.fetch(brandsQuery)
+            .then((data) => {
+                setBrands(data);
+            })
+    }, [])
+
+    const tst = testimonials[currentIndex];
+
+    return (
+        <>
+            <Swiper
+                modules={[Navigation, A11y]}
+                spaceBetween={0}
+                slidesPerView={1}
+                navigation
+                loop={true}
+            >
+                {testimonials.map((test, testIndex) => (
+                    <SwiperSlide key={testIndex}>
+                        <div className={'app__testimonial-item app__flex'}>
+                            <img src={urlFor(tst.imgurl)} alt="testimonial"/>
+                            <div className={'app__testimonial-content'}>
+                                <p className="p-text">{tst.feedback}</p>
+                                <div>
+                                    <h4 className="bold-text">{tst.name}</h4>
+                                    <h5 className="p-text">{tst.company}</h5>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+
+            <div className={'app__testimonial-brands app__flex'}>
+                {brands.map(brand => (
+                    <motion.div
+                        whileInView={{opacity: [0, 1]}}
+                        transition={{duration: .5, type: 'tween'}}
+                        key={brand._id}
+                    >
+                        <img src={urlFor(brand.imgUrl)} alt={brand.name}/>
+                    </motion.div>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default AppWrap(
+    MotionWrap(Testimonial, 'app__testimonial'),
+    'testimonials',
+    'app__primarybg'
+);
