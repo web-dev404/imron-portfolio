a9e1210d2bef62898af49bd3acea6ad3a007e939
Imron Mirzarasulov <imronimronov32@gmail.com>
2022-09-28 15:52:54 +0500

    new avatar

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,45 +1,44 @@
-import email from '../assets/email.png';
-import mobile from '../assets/mobile.png';
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
+import circle from '../assets/circle.svg';
 import css from '../assets/css.png';
+import email from '../assets/email.png';
 import figma from '../assets/figma.png';
 import html from '../assets/html.png';
 import javascript from '../assets/javascript.png';
+import logo from '../assets/logo.png';
+import mobile from '../assets/mobile.png';
+import nextjs from '../assets/nextjs.png';
+
+import profile from '../assets/profile.png';
+import profile3 from '../assets/profile3.png';
 import react from '../assets/react.png';
 import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import typescript2 from '../assets/typescript.svg';
-import nextjs from '../assets/nextjs.png';
-
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
-
-import profile from '../assets/profile.png';
-import profile2 from '../assets/profile2.png';
-import circle from '../assets/circle.svg';
-import logo from '../assets/logo.png';
 
 export default {
-    email,
-    mobile,
-    css,
-    figma,
-    html,
-    javascript,
-    react,
-    redux,
-    sass,
-    typescript,
-    typescript2,
-    nextjs,
-    about01,
-    about02,
-    about03,
-    about04,
-    profile,
-    profile2,
-    circle,
-    logo,
+	email,
+	mobile,
+	css,
+	figma,
+	html,
+	javascript,
+	react,
+	redux,
+	sass,
+	typescript,
+	typescript2,
+	nextjs,
+	about01,
+	about02,
+	about03,
+	about04,
+	profile,
+	profile3,
+	circle,
+	logo,
 };
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,77 +1,77 @@
 "use strict";
-import React from 'react';
-import './Header.scss';
 import {motion} from 'framer-motion'
-import {images} from '../../constants';
-import {AppWrap} from '../../wrapper'
+import React from 'react';
 import {FiDownload} from "react-icons/fi";
 import summary from '../../assets/summary.docx';
+import {images} from '../../constants';
+import {AppWrap} from '../../wrapper'
+import './Header.scss';
 
 const scaleVariants = {
-    whileInView: {
-        scale: [0, 1],
-        opacity: [0, 1],
-        transition: {
-            duration: 1,
-            ease: 'easeInOut'
-        }
-    }
+	whileInView: {
+		scale: [0, 1],
+		opacity: [0, 1],
+		transition: {
+			duration: 1,
+			ease: 'easeInOut'
+		}
+	}
 }
 
 const Header = () => {
-    return (
-        <div id={'home'} className={'app__header app__flex'}>
-            <motion.div
-            whileInView={{x: [-100, 0], opacity: [0, 1]}}
-            transition={{duration: .5}}
-            className={'app__header-info'}
-            >
-                <div className={'app__header-badge'}>
-                    <div className="badge-cmp app__flex">
-                        <span>👋</span>
-                        <div style={{marginLeft: 20}}>
-                            <p className="p-text">Привет, Я</p>
-                            <h1 className="head-text">Imron</h1>
-                        </div>
-                    </div>
-
-                    <div className="tag-cmp app__flex">
-                        <div className="p-text">Front-End Разработчик</div>
-                        <div className="p-text">3 года опыта</div>
-                    </div>
-
-                    <a href={summary} className={'cv-btn'}>Скачать CV <span><FiDownload/></span></a>
-                </div>
-            </motion.div>
-
-            <motion.div
-                whileInView={{opacity: [0, 1]}}
-                transition={{duration: .5, delayChildren: .5}}
-                className={'app__header-img'}
-            >
-                <img className={'profile__img'} src={images.profile2} alt="profile_bg"/>
-
-                <motion.img
-                    whileInView={{scale: [0, 1]}}
-                    transition={{duration: 1, ease: 'easeInOut'}}
-                    className={'overlay_circle'}
-                    src={images.circle}
-                    alt={'profile_circle'}
-                />
-            </motion.div>
-
-            <motion.div
-                variant={scaleVariants}
-                whileInView={scaleVariants.whileInView}
-                className={'app__header-circles'}
-            >
-                {[images.react, images.nextjs, images.typescript2].map((circle, index) => (
-                    <div key={`circle-${index}`} className={'circle-cmp app__flex'}>
-                        <img src={circle} alt="circle"/>
-                    </div>
-                ))}
-            </motion.div>
-        </div>
-    )
+	return (
+		<div id={'home'} className={'app__header app__flex'}>
+			<motion.div
+				whileInView={{x: [-100, 0], opacity: [0, 1]}}
+				transition={{duration: .5}}
+				className={'app__header-info'}
+			>
+				<div className={'app__header-badge'}>
+					<div className="badge-cmp app__flex">
+						<span>👋</span>
+						<div style={{marginLeft: 20}}>
+							<p className="p-text">Привет, Я</p>
+							<h1 className="head-text">Imron</h1>
+						</div>
+					</div>
+					
+					<div className="tag-cmp app__flex">
+						<div className="p-text">Front-End Разработчик</div>
+						<div className="p-text">3 года опыта</div>
+					</div>
+					
+					<a href={summary} className={'cv-btn'}>Скачать CV <span><FiDownload/></span></a>
+				</div>
+			</motion.div>
+			
+			<motion.div
+				whileInView={{opacity: [0, 1]}}
+				transition={{duration: .5, delayChildren: .5}}
+				className={'app__header-img'}
+			>
+				<img className={'profile__img'} src={images.profile3} alt="profile_bg"/>
+				
+				<motion.img
+					whileInView={{scale: [0, 1]}}
+					transition={{duration: 1, ease: 'easeInOut'}}
+					className={'overlay_circle'}
+					src={images.circle}
+					alt={'profile_circle'}
+				/>
+			</motion.div>
+			
+			<motion.div
+				variant={scaleVariants}
+				whileInView={scaleVariants.whileInView}
+				className={'app__header-circles'}
+			>
+				{[images.react, images.nextjs, images.typescript2].map((circle, index) => (
+					<div key={`circle-${index}`} className={'circle-cmp app__flex'}>
+						<img src={circle} alt="circle"/>
+					</div>
+				))}
+			</motion.div>
+		</div>
+	)
 }
 export default AppWrap(Header, 'home');
